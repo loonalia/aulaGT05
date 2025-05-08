c458d197b778aa5fcdcff2083591ff779fb4adfb
thaliags <thaliagsilva06@gmail.com>
2025-05-08 16:15:29 -0300

    add javascript

---
+++ index.js
@@ -0,0 +1,41 @@
+const searchInput = document.getElementById(searchInput)
+
+const produtos = [
+    {
+        img: './Imagens/papergirls1.jpeg',
+        titulo: 'Paper Girls 1',
+        categoria: 'Ficção Científica',
+        preco: 50.98,
+        desconto: true
+    }, {
+        img: './Imagens/papergirls2.jpeg',
+        titulo: 'Paper Girls 2',
+        categoria: 'Ficção Científica',
+        preco: 50.98,
+        desconto: true
+    }, {
+        img: './Imagens/papergirls3.jpeg',
+        titulo: 'Paper Girls 3',
+        categoria: 'Ficção Científica',
+        preco: 47.19,
+        desconto: false
+    }, {
+        img: './Imagens/papergirls4.jpeg',
+        titulo: 'Paper Girls 4',
+        categoria: 'Ficção Científica',
+        preco: 54.93,
+        desconto: true
+    }, {
+        img: './Imagens/papergirls5.jpeg',
+        titulo: 'Paper Girls 5',
+        categoria: 'Ficção Científica',
+        preco: 40.29,
+        desconto: true
+    }, {
+        img: './Imagens/papergirls6.jpeg',
+        titulo: 'Paper Girls 6',
+        categoria: 'Ficção Científica',
+        preco: 46.92,
+        desconto: false
+    }
+]
